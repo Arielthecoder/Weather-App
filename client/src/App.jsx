1d6eaff68bb58be818916c76a4f370471eee2042
Arielthecoder <ahermitt02@gmail.com>
2023-09-01 16:21:55 -0400

    continue fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,27 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [cityName, setCityName] = useState("Rome");
+  const [cityName, setCityName] = useState("Seattle");
   const [inputText, setInputText] = useState("");
   const [data, setData] = useState({});
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true); }
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0b3cd43b9e4773e225bf302f1e1d8fdc&units=metric`)
+
+      .then((res) => {
+        if (res.status === 200) {
+          error && setError(false);
+          return res.json();
+        } else {
+          throw new Error("Sorry something went wrong please try again.");
+        }
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, [cityName, error]);
